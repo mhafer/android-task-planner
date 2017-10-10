7b96526299897d1543f4b69ed0420956449acba2
msalema <missmichellesalema@gmail.com>
2017-10-10 11:53:17 -0400

    add custom theme

--- TaskPlanner/app/src/main/java/com/example/missm/taskplannerapp/SettingsActivity.java
+++ TaskPlanner/app/src/main/java/com/example/missm/taskplannerapp/SettingsActivity.java
@@ -5,12 +5,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 
+import com.example.missm.taskplannerapp.utilities.InterfaceUtils;
+
 public class SettingsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        // TODO: Eventually set the theme for the whole application using preferences
+        //set the status color
+        InterfaceUtils.setTranslucentStatusBar(getWindow());
 
         Toolbar myChildToolbar = (Toolbar) findViewById(R.id.setting_toolbar);
         setSupportActionBar(myChildToolbar);
